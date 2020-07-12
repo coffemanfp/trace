9d2db30c88342cd80c5d8eec2d97f06d1d9b64f6
CoffeManFP <coffemanfp@gmail.com>
2020-07-12 19:08:46 -0400

    First commit

---
+++ trace.go
@@ -0,0 +1,11 @@
+package trace
+
+import "io"
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func New(w io.Writer) {}
